02e3ca8e84d4000732ec6d30558731d01d4fb900
엽킹 <kim91star2@naver.com>
2022-09-01 22:19:25 +0900

    entity 구현

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//@Entity
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
