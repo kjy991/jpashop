6bb1ef349003408b5ad42f6d36b426b730a493f1
엽킹 <kim91star2@naver.com>
2022-09-11 16:45:29 +0900

    order 테스트 완료

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -12,15 +12,23 @@ import java.util.List;
 public class OrderRepository {
     private final EntityManager em;
 
-    public void save(Order order){
+    public void save(Order order) {
         em.persist(order);
     }
 
-    public Order  findOne(Long id) {
-        return em.find(Order.class,id);
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                        " where o.status = :status " +
+                        " and m.name like :name", Order.class)
+                .setParameter("status",orderSearch.getOrderStatus())
+                .setParameter("name",orderSearch.getMemberName())
+                .setFirstResult(100)
+                .setMaxResults(1000)
+                .getResultList();
+
+    }
 }
